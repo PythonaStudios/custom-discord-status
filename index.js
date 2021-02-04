05a04a674af9db55d0f586b2dbe295ec9ca14271
Thomas Ewan Sykes <tom@pythonastudios.com>
2021-02-04 16:39:47 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ client.on('ready', () => {
 client.request('SET_ACTIVITY', {
 pid: process.pid,
 activity : {
-details : "Up to 20% of Pythona Studios products (valid till: 1/4/2021)",
+details : "yourdescription",
 assets : {
-large_image : "pythona_studios",
-large_text : "Pythona Studios CEO" // THIS WILL SHOW AS "Playing <Status>" from the outisde
+large_image : "imagename",
+large_text : "" // THIS WILL SHOW AS "Playing <Status>" from the outisde
 },
 buttons : [{label : "WEBSITE" , url : "https://pythonastudios.com"},{label : "SERVER",url : "https://g9.yt/psdcrd"}]
 }
 })
 })
-client.login({ clientId : "804095585509572618" }).catch(console.error);
+client.login({ clientId : "ApplicationsID" }).catch(console.error);
